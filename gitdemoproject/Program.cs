8cc1b3c4a88163db601d5516088b4358be0a5b07
rishi <rishipandey7aug@gmail.com>
2021-08-19 10:39:47 +0530

    Second commit

--- gitdemoproject/Program.cs
+++ gitdemoproject/Program.cs
@@ -7,6 +7,7 @@ namespace gitdemoproject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First commit changes");
         }
     }
 }
