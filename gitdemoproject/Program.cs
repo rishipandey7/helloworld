e3d5d1961a02682af8a921ce114996fc302bdca5
rishi <rishipandey7aug@gmail.com>
2021-08-19 10:37:36 +0530

    first commit

---
+++ gitdemoproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitdemoproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
